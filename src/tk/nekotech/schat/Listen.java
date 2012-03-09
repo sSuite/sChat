fa67f30d18c54b102c8157ac6150167d9a1024d0
FunnyJ2 <Aksel@Aksel-PC.(none)>
2012-03-09 14:37:04 +0100

    playerjoin and some stuff i think

--- src/tk/nekotech/schat/Listen.java
+++ src/tk/nekotech/schat/Listen.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Listen implements Listener {
 	
@@ -18,6 +19,11 @@ public class Listen implements Listener {
 		event.setCancelled(true);
 		String formatted = plugin.getFormattedMessage(event.getPlayer(), event.getMessage());
 		plugin.getServer().broadcastMessage(formatted);
+	}
+	
+	@EventHandler
+	public void playerJoin(PlayerJoinEvent event) {
+		event.getPlayer().setDisplayName(plugin.replaceWithChatColorWithOutAsterix(plugin.getConfig().getString("default-name-color")) + event.getPlayer().getName());
 	}
 
 }
