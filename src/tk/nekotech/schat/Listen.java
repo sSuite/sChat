0086fea64f37ad5efc5a204211d248b5085802d5
Ethan <qwertyqwertyqwertyre@gmail.com>
2012-03-12 18:00:20 -1000

    Attempt to change package capitalization :3, shortened a method name, replaced * with $ for variable declaration.
    Moved a replacing method to main method.

--- src/tk/nekotech/schat/Listen.java
+++ src/tk/nekotech/schat/Listen.java
@@ -1,4 +1,4 @@
-package tk.nekotech.schat;
+package tk.nekotech.sChat;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -7,23 +7,23 @@ import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Listen implements Listener {
-	
-	private SChat plugin;
-	
-	public Listen(SChat instance) {
+
+	private final sChat plugin;
+
+	public Listen(sChat instance) {
 		this.plugin = instance;
 	}
-	
+
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
 	public void playerChat(PlayerChatEvent event) {
 		event.setCancelled(true);
 		String formatted = plugin.getFormattedMessage(event.getPlayer(), event.getMessage());
 		plugin.getServer().broadcastMessage(formatted);
 	}
-	
+
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent event) {
-		event.getPlayer().setDisplayName(plugin.replaceWithChatColorWithOutAsterix(plugin.getConfig().getString("default-name-color")) + event.getPlayer().getName());
+		event.getPlayer().setDisplayName(plugin.replacePlainChatColor(plugin.getConfig().getString("default-name-color")) + event.getPlayer().getName());
 	}
 
 }
--- src/tk/nekotech/schat/SChat.java
+++ src/tk/nekotech/schat/SChat.java
@@ -1,31 +1,31 @@
-package tk.nekotech.schat;
+package tk.nekotech.sChat;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SChat extends JavaPlugin {
-	
-	private Listen listener = new Listen(this);
-	
+public class sChat extends JavaPlugin {
+
+	private final Listen listener = new Listen(this);
+
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(listener, this);
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
 	}
-	
+
 	public void onDisable() {
-		
+
 	}
-	
+
 	public String getFormattedMessage(Player player, String message) {
 		String configStr = this.getConfig().getString("chat-format");
-		configStr = configStr.replace("*name", player.getDisplayName());
-		configStr = configStr.replace("*message", message);
-		
+		configStr = configStr.replace("$name", player.getDisplayName());
+		configStr = configStr.replace("$message", message);
+
 		//colors
 		configStr = this.replaceWithChatColor(configStr);
-		
+
 		/*
 		toFix = toFix.replace("darkgreen", ChatColor.DARK_GREEN.toString());
 		toFix = toFix.replace("darkred", ChatColor.DARK_RED.toString());
@@ -33,7 +33,7 @@ public class SChat extends JavaPlugin {
 		toFix = toFix.replace("darkblue", ChatColor.DARK_BLUE.toString());
 		toFix = toFix.replace("darkgray", ChatColor.DARK_GRAY.toString());
 		toFix = toFix.replace("darkpurple", ChatColor.DARK_PURPLE.toString());
-		
+
 		toFix = toFix.replace("red", ChatColor.RED.toString());
 		toFix = toFix.replace("gold", ChatColor.GOLD.toString());
 		toFix = toFix.replace("green", ChatColor.GREEN.toString());
@@ -44,41 +44,41 @@ public class SChat extends JavaPlugin {
 		toFix = toFix.replace("gray", ChatColor.GRAY.toString());
 		toFix = toFix.replace("purple", ChatColor.LIGHT_PURPLE.toString());
 		toFix = toFix.replace("yellow", ChatColor.YELLOW.toString());
-		*/
-		
-		//"easter egg"
-		configStr = configStr.replace("*magic", ChatColor.MAGIC.toString());
-		
+		 */
+
 		return configStr;
 	}
-	
+
 	public String replaceWithChatColor(String toFix) {
 		/*
 		 * IMPORTANT
 		 * DARK COLORS HAS TO BE FIRST OR BAD THINGS WILL HAPPEN
 		 */
-		toFix = toFix.replace("*darkgreen", ChatColor.DARK_GREEN.toString());
-		toFix = toFix.replace("*darkred", ChatColor.DARK_RED.toString());
-		toFix = toFix.replace("*darkaqua", ChatColor.DARK_AQUA.toString());
-		toFix = toFix.replace("*darkblue", ChatColor.DARK_BLUE.toString());
-		toFix = toFix.replace("*darkgray", ChatColor.DARK_GRAY.toString());
-		toFix = toFix.replace("*darkpurple", ChatColor.DARK_PURPLE.toString());
-		
-		toFix = toFix.replace("*red", ChatColor.RED.toString());
-		toFix = toFix.replace("*gold", ChatColor.GOLD.toString());
-		toFix = toFix.replace("*green", ChatColor.GREEN.toString());
-		toFix = toFix.replace("*aqua", ChatColor.AQUA.toString());
-		toFix = toFix.replace("*white", ChatColor.WHITE.toString());
-		toFix = toFix.replace("*blue", ChatColor.BLUE.toString());
-		toFix = toFix.replace("*black", ChatColor.BLACK.toString());
-		toFix = toFix.replace("*gray", ChatColor.GRAY.toString());
-		toFix = toFix.replace("*purple", ChatColor.LIGHT_PURPLE.toString());
-		toFix = toFix.replace("*yellow", ChatColor.YELLOW.toString());
-		
+		toFix = toFix.replace("$darkgreen", ChatColor.DARK_GREEN.toString());
+		toFix = toFix.replace("$darkred", ChatColor.DARK_RED.toString());
+		toFix = toFix.replace("$darkaqua", ChatColor.DARK_AQUA.toString());
+		toFix = toFix.replace("$darkblue", ChatColor.DARK_BLUE.toString());
+		toFix = toFix.replace("$darkgray", ChatColor.DARK_GRAY.toString());
+		toFix = toFix.replace("$darkpurple", ChatColor.DARK_PURPLE.toString());
+
+		toFix = toFix.replace("$red", ChatColor.RED.toString());
+		toFix = toFix.replace("$gold", ChatColor.GOLD.toString());
+		toFix = toFix.replace("$green", ChatColor.GREEN.toString());
+		toFix = toFix.replace("$aqua", ChatColor.AQUA.toString());
+		toFix = toFix.replace("$white", ChatColor.WHITE.toString());
+		toFix = toFix.replace("$blue", ChatColor.BLUE.toString());
+		toFix = toFix.replace("$black", ChatColor.BLACK.toString());
+		toFix = toFix.replace("$gray", ChatColor.GRAY.toString());
+		toFix = toFix.replace("$purple", ChatColor.LIGHT_PURPLE.toString());
+		toFix = toFix.replace("$yellow", ChatColor.YELLOW.toString());
+
+		//"easter egg"
+		toFix = toFix.replace("$magic", ChatColor.MAGIC.toString());
+
 		return toFix;
 	}
-	
-	public String replaceWithChatColorWithOutAsterix(String toFix) {
+
+	public String replacePlainChatColor(String toFix) {
 		/*
 		 * IMPORTANT
 		 * DARK COLORS HAS TO BE FIRST OR BAD THINGS WILL HAPPEN
@@ -89,7 +89,7 @@ public class SChat extends JavaPlugin {
 		toFix = toFix.replace("darkblue", ChatColor.DARK_BLUE.toString());
 		toFix = toFix.replace("darkgray", ChatColor.DARK_GRAY.toString());
 		toFix = toFix.replace("darkpurple", ChatColor.DARK_PURPLE.toString());
-		
+
 		toFix = toFix.replace("red", ChatColor.RED.toString());
 		toFix = toFix.replace("gold", ChatColor.GOLD.toString());
 		toFix = toFix.replace("green", ChatColor.GREEN.toString());
@@ -100,8 +100,8 @@ public class SChat extends JavaPlugin {
 		toFix = toFix.replace("gray", ChatColor.GRAY.toString());
 		toFix = toFix.replace("purple", ChatColor.LIGHT_PURPLE.toString());
 		toFix = toFix.replace("yellow", ChatColor.YELLOW.toString());
-		
+
 		return toFix;
 	}
-	
+
 }
