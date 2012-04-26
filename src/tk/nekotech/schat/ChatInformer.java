eebad8018f6de3a80a17eee3713a3f7931236cd5
Ethan <qwertyqwertyqwertyre@gmail.com>
2012-04-25 15:09:39 -1000

    Change in informant

--- src/tk/nekotech/schat/ChatInformer.java
+++ src/tk/nekotech/schat/ChatInformer.java
@@ -2,6 +2,7 @@ package tk.nekotech.sChat;
 
 import java.lang.reflect.Method;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tk.nekotech.sSuiteLib.ErrorHandling;
@@ -10,14 +11,13 @@ import tk.nekotech.sSuiteLib.Logging;
 public class ChatInformer{
 
 	@SuppressWarnings("unused")
-	private Class<JavaPlugin> c;
 	private ErrorHandling err;
 	private Logging log;
 	@SuppressWarnings("unused")
 	private Method m;
 	public Object oo;
 	private String[] mlist;
-	private Class<JavaPlugin>[] clist;
+	private Class<?>[] clist;
 	private int num = 0;
 
 	public ChatInformer(SChat p){
@@ -25,8 +25,7 @@ public class ChatInformer{
 		this.log = new Logging(p, SChat.cfg, "debugMode", "silentMode");
 	}
 
-	public String getListener(JavaPlugin jp, Class<JavaPlugin> c, String method){
-		this.c = c;
+	public int getListener(JavaPlugin jp, Class<?> c, String method){
 		String pname = jp.getDescription().getName();
 		try{
 			m = c.getDeclaredMethod(method);
@@ -35,15 +34,21 @@ public class ChatInformer{
 		}catch (NoSuchMethodException e){
 			err.PST(e);
 		}
-		for (Class<JavaPlugin> i : clist){
+		for (Class<?> i : clist){
 			if (i.equals(c)){
 				log.severe("Chat listener already created for plugin \"" + pname + "\"!");
-				return "0";
+				return 0;
 			}
 		}
 		clist[num] = c;
 		mlist[num] = method;
 		num = num + 1;
-		return "1";
+		return 1;
+	}
+
+	public void informChat(Player p, String m, String t){
+		for (Class<?> c : clist){
+
+		}
 	}
 }
